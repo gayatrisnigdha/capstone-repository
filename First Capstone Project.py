2baf8b47307fe6a7d0eab709011824dfb7e8b68e
gayatrisnigdha <48373317+gayatrisnigdha@users.noreply.github.com>
2019-12-11 11:32:51 +0530

    Add files via upload

---
+++ First Capstone Project.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# We are mainly using this notebook for Capstone Project.
+# 
+# 
+# 
+
+# First, let us import pandas and numpy libraries.
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
